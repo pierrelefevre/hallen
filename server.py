31d50093860e9ebb8151c03242fc335beebd14d8
Pierre Le Fevre <hello@pierrelf.com>
2023-06-08 10:00:45 +0200

    Basic page

--- server.py
+++ server.py
@@ -1,17 +1,29 @@
 import os
 import subprocess
-from flask import Flask, send_file
+import threading
+from flask import Flask, send_file, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='/home/pi/hallen')
+latest_image_path = "/home/pi/hallen/output.png"
 
 def capture_image():
-    command = ["fswebcam", "--no-banner", "--quiet", "--resolution", "960x720", "/home/pi/hallen/output.png"]
-    subprocess.run(command, check=True)
+    command = ["fswebcam", "--title", "nybodahallen.se", "--timestamp", "%Y-%m-%d %H:%M:%S", "--quiet", "--delay", "1", "--resolution", "960x720", latest_image_path]
+
+    while True:
+        subprocess.run(command, check=True)
 
 @app.route('/')
+def serve_index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/output.png')
 def serve_image():
-    capture_image()
-    return send_file('/home/pi/hallen/output.png', mimetype='image/png')
+    try:
+        return send_file(latest_image_path, mimetype='image/png')
+    except Exception as e:
+        return str(e)
 
 if __name__ == "__main__":
+    thread = threading.Thread(target=capture_image, daemon=True)
+    thread.start()
     app.run(host='0.0.0.0', port=8080)
